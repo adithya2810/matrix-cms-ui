494de8ec1e074bc5543820dd94ac21724a73f2ad
Gyan Vivek <gyan.vivek@knolskape.com>
2021-05-14 16:54:10 +0530

    added coloros and typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,28 @@ module.exports = {
   purge: ["./pages/**/*.tsx", "./src/**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      primary: {
+        light: "#2C2E30",
+        DEFAULT: "#1B1919",
+        dark: "#000000",
+      },
+      secondary: {
+        light: "#FFFFFF",
+        DEFAULT: "#EBEBE9",
+      },
+      accent: {
+        light: "#0EAEB1",
+        DEFAULT: "#01576E",
+        dark: "#083A4A",
+      },
+      cta: {
+        DEFAULT: "#5DE2A1",
+      },
+    },
+    fontFamily: {
+      sans: ["Manrope", "Helvetica", "Arial", "sans-serif"],
+    },
     extend: {},
   },
   variants: {

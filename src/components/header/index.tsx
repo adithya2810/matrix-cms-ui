932674ece1748334e31ab22aae42186afc1cea8e
Gyan Vivek <gyan.vivek@knolskape.com>
2021-05-20 12:23:20 +0530

    Header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,8 +4,12 @@ import { Logo } from "@components";
 
 export const Header: React.FC = () => {
   return (
-    <div className="text-center bg-gray-800">
+    <div className="flex justify-between items-start laptop:ml-14 laptop:pl-8 mt-11 sm:ml-7">
       <Logo />
+      <div className="flex mt-2 items-center justify-start">
+        <h6 className="sub-h1 pr-1 menu-text">Menu</h6>
+        <img src="/icons/menu.svg" className="pl-2 laptop:mr-20 sm:mr-6"></img>
+      </div>
     </div>
   );
 };

30e4cae0b7205c54176f7ab4809987b4f5be542a
Gyan Vivek <gyan.vivek@knolskape.com>
2021-06-13 10:58:50 +0530

    fix header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -9,16 +9,19 @@ export type Props = {
 export const Header: React.FC<Props> = ({ toggle }) => {
   return (
     <div className="flex absolute top-0 left-0 w-full justify-between items-start">
-      <Logo />
-      <div
-        className="flex  items-center justify-start mt-2 text-accent "
-        onClick={() => toggle()}
-      >
-        <h6 className="sub-h1 pr-1 menu-text">Menu</h6>
+      <Image
+        className="header-logo"
+        src="/icons/matrixLogo.svg"
+        alt="company-logo"
+      />
+      <div className="header-menu flex items-center " onClick={() => toggle()}>
+        <span className="header-menu-text">Menu</span>
         <Image
           src="/icons/menu.svg"
           alt="menu"
-          className="pl-2 laptop:mr-20 sm:mr-6 text-blue"
+          height={24}
+          width={24}
+          style={{ marginLeft: 12 }}
         ></Image>
       </div>
     </div>
--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,14 +1,24 @@
-import React from "react";
-import Image from "next/image";
+import React from 'react';
+import Image from 'next/image';
 
 export type Props = {
-  height? : number;
-  width? : number;
-  className? : string;
-}
+  height?: number;
+  width?: number;
+  className?: string;
+};
 
-export const Logo: React.FC<Props> = ({height=65,width=156,className=""}) => {
+export const Logo: React.FC<Props> = ({
+  height = 65,
+  width = 156,
+  className = '',
+}) => {
   return (
-    <Image  src="/icons/matrixLogo.svg" alt="nextjs" width={width} height={height} className={`${className}company-logo`}/>
+    <Image
+      src="/icons/matrixLogo.svg"
+      alt="nextjs"
+      width={width}
+      height={height}
+      className={`${className} company-logo`}
+    />
   );
 };

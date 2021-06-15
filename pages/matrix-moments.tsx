f5b520a0b8e1ae7720acbc58dad86641e20a8477
Harish Ponna <harishponna55@gmail.com>
2021-06-15 22:14:25 +0530

    fixed prop types in matrix momentspage

--- pages/matrix-moments.tsx
+++ pages/matrix-moments.tsx
@@ -1,16 +1,22 @@
-import PageTitle from '@components/PageTitle'
-import MatrixMoments from '@modules/MatrixMoments/index'
-import React, { FC } from 'react'
+import PageTitle from '@components/PageTitle';
+import MatrixMoments from '@modules/MatrixMoments/index';
+import React, { FC } from 'react';
 
+type deviceType = {
+  mobile: Boolean;
+};
 
+type propsType = {
+  deviceType: deviceType;
+};
 
-const MatrixMomentsPage: FC = props => {
+const MatrixMomentsPage: FC<propsType> = (props) => {
   return (
     <>
       <PageTitle>Matrix Moments</PageTitle>
       <MatrixMoments {...props} />
     </>
-  )
-}
+  );
+};
 
-export default MatrixMomentsPage
+export default MatrixMomentsPage;
--- src/components/Listing/Icon.tsx
+++ src/components/Listing/Icon.tsx
@@ -1,17 +1,15 @@
-import { MicLaptop, MicMobile } from '@components/Icons'
-import React from 'react'
-
+import { MicLaptop, MicMobile } from '@components/Icons';
+import React from 'react';
 
 const MobileIcons = {
-  mic: <MicMobile />
-}
+  mic: <MicMobile />,
+};
 const LaptopIcons = {
-  mic: <MicLaptop />
-}
+  mic: <MicLaptop />,
+};
 
 const Icon = ({ deviceType, iconType }) => {
-  if (deviceType.mobile) return MobileIcons[iconType]
-  else return LaptopIcons[iconType]
-}
+  if (deviceType.mobile) return MobileIcons[iconType];
+  else return LaptopIcons[iconType];
+};
 export default Icon;
-

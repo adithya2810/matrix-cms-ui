deb684b59f4e91149e8d4e12afaedee529b2cc2f
Harish Ponna <harishponna55@gmail.com>
2021-06-15 22:16:38 +0530

    fixed prop types in matrix momentspage

--- src/components/Listing/List.tsx
+++ src/components/Listing/List.tsx
@@ -1,14 +1,25 @@
-import React, { FC } from 'react'
-import Card from './Card'
+import React, { FC } from 'react';
+import Card from './Card';
 
-const data = [{}, {}, {}, {}, {}, {}, {}, {},]
+const data = [{}, {}, {}, {}, {}, {}, {}, {}];
 
-const List: FC = (props) => {
+type deviceType = {
+  mobile: Boolean;
+};
+
+type propsType = {
+  pageType: String;
+  deviceType: deviceType;
+};
+
+const List: FC<propsType> = (props) => {
   return (
-    <div className='section grid grid-cols-1 laptop:grid-cols-2 gap-12 sm:gap-4'>
-      {data.map((d, i) => <Card key={i} {...props} />)}
+    <div className="section grid grid-cols-1 laptop:grid-cols-2 gap-12 sm:gap-4">
+      {data.map((d, i) => (
+        <Card key={i} {...props} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
--- src/modules/MatrixMoments/index.tsx
+++ src/modules/MatrixMoments/index.tsx
@@ -1,20 +1,29 @@
-import React, { FC } from 'react'
-import Listing from '@components/Listing'
+import React, { FC } from 'react';
+import Listing from '@components/Listing';
 
 const data = {
   pageType: 'matrix_moments',
-  title: <h2>The Matrix <br /> Moments Series</h2>
-}
+  title: (
+    <h2>
+      The Matrix <br /> Moments Series
+    </h2>
+  ),
+};
 
+type deviceType = {
+  mobile: Boolean;
+};
 
+type propsType = {
+  deviceType: deviceType;
+};
 
-const index: FC = (props) => {
-
+const index: FC<propsType> = (props) => {
   return (
     <div>
       <Listing {...props} {...data} />
     </div>
-  )
-}
+  );
+};
 
-export default index
+export default index;

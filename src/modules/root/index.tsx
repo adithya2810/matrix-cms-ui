b2161c03e8c28aafa1c121d55a4d733c801d8604
Harish Ponna <harishponna55@gmail.com>
2021-06-16 11:10:34 +0530

    fixed footer

--- src/modules/root/index.tsx
+++ src/modules/root/index.tsx
@@ -1,16 +1,16 @@
-import React, { useEffect } from "react";
-import { Layout } from "@components/layout";
-import { Menu } from "../menu";
-import { useQuery } from "@apollo/client";
+import React, { useEffect } from 'react';
+import { Layout } from '@components/layout';
+import { Menu } from '../menu';
+import { useQuery } from '@apollo/client';
 import {
   appConfigMutation,
   appConfiqQuery,
-} from "../../../operations/appConfig";
-import NProgress from "nprogress";
-import "nprogress/nprogress.css";
-import Router from "next/router";
-import { Footer } from "@components/footer";
-import { Header } from "@components/header";
+} from '../../../operations/appConfig';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+import Router from 'next/router';
+import { Footer } from '@components/footer';
+import { Header } from '@components/header';
 
 export type Props = {
   className?: string;
@@ -27,23 +27,21 @@ export const Root: React.FC<Props> = ({ children }) => {
     data: { appConfig: navMenuState },
   } = useQuery(appConfiqQuery.GET_NAV_MENU_STATE);
   useEffect(() => {
-    Router.events.on("routeChangeStart", (url) => {
+    Router.events.on('routeChangeStart', (url) => {
       NProgress.start();
     });
-    Router.events.on("routeChangeComplete", () => NProgress.done());
-    Router.events.on("routeChangeError", () => NProgress.done());
+    Router.events.on('routeChangeComplete', () => NProgress.done());
+    Router.events.on('routeChangeError', () => NProgress.done());
     NProgress.configure({ showSpinner: false });
   }, []);
-  const className = !navMenuState.menu
-    ? "visible"
-    : "invisible hidden ";
+  const className = !navMenuState.menu ? 'visible' : 'invisible hidden ';
 
   return (
     <>
       <div className={`flex flex-col flex-grow w-full h-full  ${className}`}>
-        {/* <Header toggle={appConfigMutation.toogleMenu} /> */}
+        <Header toggle={appConfigMutation.toogleMenu} />
         <div className="flex-grow">{children}</div>
-        {/* <Footer /> */}
+        <Footer />
       </div>
       <Menu />
     </>

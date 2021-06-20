5224c89c228bac9c2284a18a77fa6bb2f2259a31
MitJ <jayanimit2111@gmail.com>
2021-06-20 17:42:08 +0530

    Add Client on PortFolio

--- src/modules/portfolio/currentInvestment/currnetInvestment.tsx
+++ src/modules/portfolio/currentInvestment/currnetInvestment.tsx
@@ -8,7 +8,7 @@ const CurrentInvestment = () => {
         <h2 className="text-3xl sm:text-2xl text-center font-bold text-current">Select  Current Investments</h2>
       </div>
       <div className="grid grid-cols-4 my-24 ml-5 mr-5 lg:h-96 sm:h-auto mg:grid-cols-3 sm:grid-cols-1 gap-4">
-        <div className="flex relative self-center justify-center ">
+        {/* <div className="flex relative self-center justify-center ">
           <div className="flip-card">
             <div className="flip-card-inner ">
               <div className="flip-card-front">
@@ -21,8 +21,8 @@ const CurrentInvestment = () => {
               </div>
             </div>
           </div>
-        </div>
-        <div className="flex self-center justify-center">
+        </div> */}
+        {/* <div className="flex self-center justify-center">
           <div className="flip-card">
             <div className="flip-card-inner ">
               <div className="flip-card-front">
@@ -35,7 +35,7 @@ const CurrentInvestment = () => {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
         <div className="flex self-center justify-center">
           <div className="flip-card">
             <div className="flip-card-inner ">
@@ -58,37 +58,6 @@ const CurrentInvestment = () => {
               </div>
               <div className="flip-card-back text-left p-4">
                 <img className="w-6/12 sm:hidden md:hidden lg:hidden pb-5" src="../../img/avail.png" alt="" />
-                <p className="text-base tablet:text-xs md:text-xs sm:text-base pb-5 mb-2 text-left border-b-2">Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.</p>
-                <a href="#" className="web-url pt-4">Visit Webiste</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="grid grid-cols-4 my-24	ml-5 mr-5 lg:h-96 sm:h-auto sm:grid-cols-1 gap-4">
-
-        <div className="flex self-center justify-center">
-          <div className="flip-card">
-            <div className="flip-card-inner ">
-              <div className="flip-card-front">
-                <img className="w-6/12 	" src="../../img/itz-01.png" alt="" />
-              </div>
-              <div className="flip-card-back text-left p-4">
-                <img className="w-6/12 sm:hidden md:hidden lg:hidden pb-5" src="../../img/itz-01.png" alt="" />
-                <p className="text-base tablet:text-xs md:text-xs sm:text-base pb-5 mb-2 text-left border-b-2">Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.</p>
-                <a href="#" className="web-url pt-4">Visit Webiste</a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="flex self-center justify-center">
-          <div className="flip-card">
-            <div className="flip-card-inner ">
-              <div className="flip-card-front">
-                <img className="w-6/12 	" src="../../img/Myra.png" alt="" />
-              </div>
-              <div className="flip-card-back text-left p-4">
-                <img className="w-6/12 sm:hidden md:hidden lg:hidden pb-5" src="../../img/Myra.png" alt="" />
                 <p className="text-base tablet:text-xs md:text-xs sm:text-base pb-5 mb-2 text-left border-b-2">Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.</p>
                 <a href="#" className="web-url pt-4">Visit Webiste</a>
               </div>
@@ -124,13 +93,45 @@ const CurrentInvestment = () => {
             </div>
           </div>
         </div>
-
-
-
       </div>
+      {/* <div className="grid grid-cols-4 my-24	ml-5 mr-5 lg:h-96 sm:h-auto sm:grid-cols-1 gap-4"> */}
+
+      {/* <div className="flex self-center justify-center">
+          <div className="flip-card">
+            <div className="flip-card-inner ">
+              <div className="flip-card-front">
+                <img className="w-6/12 	" src="../../img/itz-01.png" alt="" />
+              </div>
+              <div className="flip-card-back text-left p-4">
+                <img className="w-6/12 sm:hidden md:hidden lg:hidden pb-5" src="../../img/itz-01.png" alt="" />
+                <p className="text-base tablet:text-xs md:text-xs sm:text-base pb-5 mb-2 text-left border-b-2">Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.</p>
+                <a href="#" className="web-url pt-4">Visit Webiste</a>
+              </div>
+            </div>
+          </div>
+        </div> */}
+      {/* <div className="flex self-center justify-center">
+          <div className="flip-card">
+            <div className="flip-card-inner ">
+              <div className="flip-card-front">
+                <img className="w-6/12 	" src="../../img/Myra.png" alt="" />
+              </div>
+              <div className="flip-card-back text-left p-4">
+                <img className="w-6/12 sm:hidden md:hidden lg:hidden pb-5" src="../../img/Myra.png" alt="" />
+                <p className="text-base tablet:text-xs md:text-xs sm:text-base pb-5 mb-2 text-left border-b-2">Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.</p>
+                <a href="#" className="web-url pt-4">Visit Webiste</a>
+              </div>
+            </div>
+          </div>
+        </div> */}
+
+
+
+
+      {/* </div> */}
       <div className="grid grid-cols-4 my-24 ml-5 mr-5 lg:h-96 sm:h-auto sm:grid-cols-1 gap-4">
 
-
+        {/*
         <div className="flex self-center sm:mt-24 sm:mb-24  justify-center">
           <div className="flip-card">
             <div className="flip-card-inner ">
@@ -172,7 +173,7 @@ const CurrentInvestment = () => {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
         <div className="flex self-center sm:mt-24 sm:mb-24  justify-center">
           <div className="flip-card">
             <div className="flip-card-inner ">

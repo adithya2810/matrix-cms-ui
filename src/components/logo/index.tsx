620a0cda1dee14d926561f0f2ab354a526209dee
SunilKumar02 <nfcodebase@gmail.com>
2021-06-29 20:10:29 +0530

    Image fix

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -3,10 +3,10 @@ import { Image } from "@components";
 
 export const Logo: React.FC = () => {
 
- const siteLogo = '/icons/matrixLogo.svg';
+  const siteLogo = '/icons/matrixLogo.svg';
 
   return (
     // <Image src="/icons/matrixLogo_White.svg" alt="nextjs" width="156.19px" height="65px" className="company-logo" />
-    <Image src={siteLogo} alt="nextjs" height="65" width="156" className="company-logo noMargin" />
+    <Image src={siteLogo} alt="nextjs" width={156} height={65} className="company-logo noMargin" />
   );
 };

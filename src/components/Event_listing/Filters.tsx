87725d2142613fb35c7375f08c7d8a6552ea49aa
kodeerkarthik <kkrishnegowdaostb1@gmail.com>
2021-07-12 17:57:58 +0530

    search component removed

--- src/components/Event_listing/Filters.tsx
+++ src/components/Event_listing/Filters.tsx
@@ -50,12 +50,34 @@ const Filters = ({ deviceType }) => {
   return (
     <>
       {/* Mask */}
-      {isFilterBoxOpen && (
+      {/* {isFilterBoxOpen && (
         <div className="fixed z-10 top-0 left-0 h-screen w-full bg-black opacity-40 overflow-y-hidden" />
-      )}
+      )} */}
 
       <div className="fixed laptop:right-14 z-50 laptop:top-36 sm:bottom-8 sm:left-8 sm:right-8 overflow-y-hidden">
-        {!isFilterBoxOpen ? (
+        <div
+          className="closed duration-300 w-28 cursor-pointer sm:flex sm:w-full"
+          onClick={handleFilter}
+        >
+          {/* <div className="search h-24 sm:h-12 sm:w-12  bg-accent flex justify-center items-center">
+              {SearchIcon}
+            </div>
+            <div className="filter flex laptop:flex-col sm:flex-row-reverse laptop:justify-evenly sm:justify-center items-center laptop:h-96 sm:h-12 sm:flex-grow    bg-accent-dark">
+              <div className="sub-h1 laptop:transform laptop:-rotate-90 relative laptop:top-10 text-white sm:ml-4">
+                Filters
+              </div>
+              {FilterIcon}
+            </div> */}
+          <div className="relative  bg-white laptop:h-24 sm:w-10 flex justify-center items-center">
+            {LinkedInIcon}
+            <span className="absolute laptop:bottom-0 sm:right-0 laptop:w-full sm:h-full laptop:h-1/2 sm:w-1/2 laptop:border-b-4 laptop:border-l-4 laptop:border-r-4 sm:border-t-2 sm:border-r-2 sm:border-b-2 border-accent" />
+          </div>
+          <div className="relative bg-white laptop:h-24 flex sm:w-10 justify-center items-center">
+            {TwitterIcon}
+            <span className="absolute laptop:bottom-0 sm:right-0 laptop:w-full sm:h-full laptop:h-1/2 sm:w-1/2 laptop:border-b-4 laptop:border-l-4 laptop:border-r-4 sm:border-t-2 sm:border-r-2 sm:border-b-2 border-accent" />
+          </div>
+        </div>
+        {/* {!isFilterBoxOpen ? (
           // when filter box is closed state
           <div
             className="closed duration-300 w-28 cursor-pointer sm:flex sm:w-full"
@@ -64,12 +86,12 @@ const Filters = ({ deviceType }) => {
             <div className="search h-24 sm:h-12 sm:w-12  bg-accent flex justify-center items-center">
               {SearchIcon}
             </div>
-            {/* <div className="filter flex laptop:flex-col sm:flex-row-reverse laptop:justify-evenly sm:justify-center items-center laptop:h-96 sm:h-12 sm:flex-grow    bg-accent-dark">
+            <div className="filter flex laptop:flex-col sm:flex-row-reverse laptop:justify-evenly sm:justify-center items-center laptop:h-96 sm:h-12 sm:flex-grow    bg-accent-dark">
               <div className="sub-h1 laptop:transform laptop:-rotate-90 relative laptop:top-10 text-white sm:ml-4">
                 Filters
               </div>
               {FilterIcon}
-            </div> */}
+            </div>
             <div className="relative  bg-white laptop:h-24 sm:w-10 flex justify-center items-center">
               {LinkedInIcon}
               <span className="absolute laptop:bottom-0 sm:right-0 laptop:w-full sm:h-full laptop:h-1/2 sm:w-1/2 laptop:border-b-4 laptop:border-l-4 laptop:border-r-4 sm:border-t-2 sm:border-r-2 sm:border-b-2 border-accent" />
@@ -108,7 +130,7 @@ const Filters = ({ deviceType }) => {
             <div
               className={`transition-colors duration-700 filter-list px-14 py-10 sm:px-7 sm:py-5 ${inputText ? 'bg-input' : 'bg-accent-dark'
                 }`}
-              style={{ height: deviceType.mobile ? 400 : 570 }}
+              style={{ height: deviceType.mobile ? 400 : 370 }}
             >
               <ul className="sub-h1 text-white">
                 <li className="py-4 sm:py-3  cursor-pointer hover:text-accent-light transition-colors duration-100">
@@ -137,7 +159,7 @@ const Filters = ({ deviceType }) => {
               </div>
             </div>
           </div>
-        )}
+        )} */}
       </div>
     </>
   );

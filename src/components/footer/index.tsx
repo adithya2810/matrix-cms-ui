f3171e111a3010677f565191f373f5e4bc659740
kamal <adithya2801@gmail.com>
2021-07-22 13:20:55 +0530

    footer letterspacing, input size and background color
    1. footer subscriber input box size increase
    2. subscriber top text letterspacing
    3. footer right texts letterspacing

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -14,15 +14,15 @@ export const Footer: React.FC = () => {
             <div className="footer-col01-left-section">
               <h6 className="-mt-10 laptop:w-48 font-normal leading-9 text-4xl tracking-wider p-0.5" style={{ fontSize: 32, lineHeight: '36px' }}> Let's stay engaged</h6>
               <span className="p-1 ml-4">
-                <h6 className="sub-h2 font-medium text-lg leading-6" style={{ lineHeight: '24px', letterSpacing: 1 }}>Sign up for the Matrix Moments series</h6>
-                <input className="mt-2 text-secondary bg-accent p-2 pl-3 w-full" style={{ color: "#FBF9F5" }} type="email" placeholder="Your email address goes here" />
+                <h6 className="sub-h2 font-medium text-lg leading-6 mr-12" style={{ lineHeight: '24px', letterSpacing: 1 }}>Sign up for the Matrix Moments series</h6>
+                <input className="mt-2 text-secondary bg-accent p-2 pl-3 w-full" style={{ color: "#FBF9F5", backgroundColor: '#fbf9f514' }} type="email" placeholder="Your email address goes here" />
                 <Button title="Subscribe" url="/icons/arrow.svg" className={"p-1 text-cta"} onClick={() => console.log("subscribe")} style={{ fontSize: 18, lineHeight: '24px', letterSpacing: 1 }} />
               </span>
             </div>
             <div className="p-0.5 sm:flex sm:justify-between ">
               <div>
-                <h6 className="sub-h2 font-medium text-lg"> Matrix Partner Us </h6>
-                <h6 className=" sub-h2 font-medium text-lg"> Matrix Partner China</h6>
+                <h6 className="sub-h2 font-medium text-lg" style={{ letterSpacing: 1 }}> Matrix Partner Us </h6>
+                <h6 className=" sub-h2 font-medium text-lg" style={{ letterSpacing: 1 }}> Matrix Partner China</h6>
               </div>
               <div className="flex mt-2 laptop:mt-4 items-start p-0.5">
                 <a className="pl-0"><img src="/icons/linkedin.svg" /></a>
@@ -38,7 +38,7 @@ export const Footer: React.FC = () => {
           <div className="footer-section-col02-inner laptop:flex laptop:items-center" style={{ padding: '0 30px' }}>
             <FooterMenu />
             <div className="privacyMenuWrap text-secondary flex items-center sm:mt-2 sm:grid sm:grid-cols-2 sm:gap-0  laptop:ml-20 ">
-              <span className="caption p-1 w-28 text-sm sm:mt-2" >PRIVACY POLICY</span>
+              <span className="caption p-1 w-28 text-sm sm:mt-2" style={{ letterSpacing: 1 }}>PRIVACY POLICY</span>
             </div>
           </div>
         </div>

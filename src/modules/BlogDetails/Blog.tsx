2380fe796270cad4f58dcd786cce660a79bcf19b
Harish Ponna <harishponna55@gmail.com>
2021-07-29 20:27:25 +0530

    blog fix

--- src/modules/BlogDetails/Blog.tsx
+++ src/modules/BlogDetails/Blog.tsx
@@ -13,14 +13,19 @@ type propsType = {
 
 const contentType = {
   video: 'youtube_embed',
-  audio: 'youtube_embed',
+  audio: 'audioboomembed',
 }
 
 
 const Blog: FC<propsType> = ({ deviceType: { mobile }, blogDetails }) => {
+  const typeObj = {
+    Audio: 'listen',
+    video: 'watch',
+    Article: 'read'
+  }
   return (
     <div className='col-span-8 laptop:mr-36 relative laptop:mt-16 sm:mt-10' style={{ minHeight: 300 }}>
-      <div className='absolute right-0 top-0'><div className='absolute bottom-0 sm:bg-accent laptop:bg-accent-dark text-white right-0 laptop:p-3 sm:p-1 sub-h2 flex justify-center items-center' style={{ width: !mobile ? 170 : 120 }}>{blogDetails?.readtime} watch</div></div>
+      <div className='absolute right-0 top-0'><div className='absolute bottom-0 sm:bg-accent laptop:bg-accent-dark text-white right-0 laptop:p-3 sm:p-1 sub-h2 flex justify-center items-center' style={{ width: !mobile ? 170 : 120 }}>{blogDetails?.readtime} {typeObj[blogDetails?.type]}</div></div>
       {
         <div className='blog-content w-full mb-10' dangerouslySetInnerHTML={{ __html: blogDetails?.[contentType[blogDetails.type]] }} />
       }

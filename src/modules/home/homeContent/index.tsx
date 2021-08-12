2e8bd8f13392eb40cf307221d912340e7351b007
kamal <adithya2801@gmail.com>
2021-08-12 17:20:22 +0530

    home page content change card filter by type to content_type

--- src/modules/home/homeContent/index.tsx
+++ src/modules/home/homeContent/index.tsx
@@ -43,11 +43,11 @@ const HomeContent: React.FC<{ mobile: boolean }> = ({ mobile }) => {
   const componentDidMount = async () => {
     let url;
     if (selectedTab == "blogs") {
-      url = 'http://ec2-3-108-61-121.ap-south-1.compute.amazonaws.com:1337/blogs?type=Article';
+      url = 'http://ec2-3-108-61-121.ap-south-1.compute.amazonaws.com:1337/blogs?content_type.name=Article';
     } else if (selectedTab == "podcast") {
-      url = 'http://ec2-3-108-61-121.ap-south-1.compute.amazonaws.com:1337/blogs?type=Audio';
+      url = 'http://ec2-3-108-61-121.ap-south-1.compute.amazonaws.com:1337/blogs?content_type.name=Audio';
     } else if (selectedTab == "videocasts") {
-      url = 'http://ec2-3-108-61-121.ap-south-1.compute.amazonaws.com:1337/blogs?type=video';
+      url = 'http://ec2-3-108-61-121.ap-south-1.compute.amazonaws.com:1337/blogs?content_type.name=Video';
     }
     const response = await fetch(url);
     const json = await response.json();

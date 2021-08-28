876e26f0ae45ae04c8259646d980e25735c38eee
komalsaini63 <komalsaini2010@gmail.com>
2021-08-28 12:24:07 +0530

    linkedin click problem in news and blog details

--- src/components/Listing/Filters.tsx
+++ src/components/Listing/Filters.tsx
@@ -89,7 +89,7 @@ const Filters = ({ deviceType }) => {
               placeholder="search for more blogs"
             />
           </div>
-          <a className="relative bg-white laptop:h-16 sm:w-10 flex justify-center items-center" onClick={() => window.location.href = "https://www.linkedin.com/company/matrix-partners/"} style={deviceType.mobile ? {} : { width: 105, height: 70 }} href="https://www.linkedin.com/company/matrix-partners/" target="__blank">
+          <a className="relative bg-white laptop:h-16 sm:w-10 flex justify-center items-center" style={deviceType.mobile ? {} : { width: 105, height: 70 }} href="https://www.linkedin.com/company/matrix-partners/" target="__blank">
             {LinkedInIcon}
             <span className="absolute laptop:bottom-0 sm:right-0 laptop:w-full sm:h-full laptop:h-1/2 sm:w-1/2 laptop:border-b-2 laptop:border-l-2 laptop:border-r-2 sm:border-t-2 sm:border-r-2 sm:border-b-2 border-accent" />
           </a>
